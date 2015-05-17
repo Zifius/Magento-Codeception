4b2ba579b7ca756f0fc9373c36b542339b186137
Alexander Turiak <alex@hexbrain.com>
2015-05-17 12:39:29 +0200

    Initial Checkout tests implementation

---
+++ README.md
@@ -0,0 +1,18 @@
+# Magento CodeCeption Tests
+
+## Prerequisites
+
+* Granted [HomeBrew](http://homebrew.sh) is installed, execute:
+    `brew install codecept`
+* For Selenium:
+    `brew install selenium-server-standalone` and optionally `brew install chromedriver` if you want to test with Google Chrome
+* PhantomJS:
+    `brew install phantomjs`
+
+## Running tests
+
+* Execute `selenium-server -p 4444` or `phantomjs --webdriver=4444 --ignore-ssl-errors=true` in a separate terminal tab
+* Then execute `codecept run acceptance --steps`
+
+See [Running Tests](http://codeception.com/docs/02-GettingStarted#Running-Tests) for detailed info
+
---
+++ tests/acceptance/CheckoutCest.php
@@ -0,0 +1,118 @@
+<?php
+
+use \AcceptanceTester as AT;
+
+// @codingStandardsIgnoreFile
+
+class CheckoutCest
+{
+    public function _before(AT $I)
+    {
+
+    }
+
+    public function _after(AT $I)
+    {
+
+    }
+
+    /**
+     * Tests add product to cart
+     *
+     * @group checkout
+     *
+     * @param $I \AcceptanceTester
+     *
+     */
+    public function testAddProductToCart(AT $I)
+    {
+        $I->am('Visitor');
+        $I->wantTo('Add product to cart');
+
+        $I->amGoingTo('open category page');
+        $I->amOnPage('/furniture.html');
+        $I->expectTo('see category page');
+
+        $I->amGoingTo('open product page');
+        $I->click('ul.products-grid.first li.first a');
+        $I->expectTo('see product page');
+        $I->seeElement('.catalog-product-view');
+
+        $I->amGoingTo('submit the form');
+        $I->submitForm('#product_addtocart_form', array());
+
+        $I->expectTo('see the cart page');
+        $I->seeCurrentUrlEquals('/checkout/cart/');
+        $I->expectTo('see a success message');
+        $I->seeElement('li.success-msg');
+    }
+
+    /**
+     * Tests One Page Checkout
+     *
+     * @group checkout
+     *
+     * @param $I \AcceptanceTester
+     *
+     * @depends testAddProductToCart
+     */
+    public function testOnePageCheckout(AT $I)
+    {
+        $I->am('Guest Customer');
+        $I->wantTo('use One Page Checkout');
+        $I->lookForwardTo('experience flawless checkout');
+        $I->amGoingTo('place an order as a guest');
+        $I->amOnPage('/checkout/onepage/');
+
+        $I->amGoingTo('select the checkout type');
+        $I->selectOption('#login:guest', 'guest');
+        $I->click('button#onepage-guest-register-button', '#checkout-step-login');
+
+        $I->amGoingTo('fill my address');
+        $I->fillField('#billing:firstname', 'Acceptance');
+        $I->fillField('#billing:lastname', 'Tester');
+        $I->fillField('#billing:email', 'codeception@test.com');
+        $I->selectOption('#billing:country_id', 'GB');
+        $I->fillField('#billing:street1', 'Baker Str. 1');
+        $I->fillField('#billing:postcode', '10000');
+        $I->fillField('#billing:city', 'London');
+        $I->fillField('#billing:telephone', '911');
+        $I->click('button', '#billing-buttons-container');
+
+        // TODO: Implement your shipping methods selection here
+        /*
+        $I->amGoingTo('select shipping method');
+        $I->selectOption('input[name="shipping_method"]', '?');
+        */
+
+        $I->waitForElementVisible('#shipping-method-buttons-container');
+        $I->click('button', '#shipping-method-buttons-container');
+
+        $I->waitForElementVisible('#payment-buttons-container');
+        $I->amGoingTo('select payment method');
+        $I->click('#p_method_checkmo');
+        $I->click('button', '#payment-buttons-container');
+
+        $I->waitForElementVisible('#checkout-review-submit');
+        $I->amGoingTo('review and finish my order');
+        $I->click('button', '#checkout-review-submit');
+        $I->wait(7);
+    }
+
+    /**
+     * Tests Checkout Success page
+     * @group checkout
+     *
+     * @param $I AcceptanceTester
+     *
+     * @depends testOnePageCheckout
+     */
+    public function testOnePageCheckoutSuccess(AT $I)
+    {
+        $I->wantTo('Observe the order success page');
+        $I->expectTo('see order success page');
+        $I->seeInCurrentUrl('/checkout/onepage/success');
+        $I->expectTo('be a happy customer');
+    }
+
+}
